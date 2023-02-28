2f29964b4a758d7dc557e740723108eab62238a7
Tamas Baranyai <thamasb@gmail.com>
2023-02-28 12:28:45 +0000

    Add js quiz content

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,62 +1,62 @@
 const zodiac = [
 {
     "question": "Aries",
-    "choices": [],
-    "correct":"March 21-April 19"
+    "choices": ["March 21-April 19","April 20-May 20","May 21-June 20"],
+    "correct":"March 21-April 19",
 },
 {
     "question": "Taurus",
-    "choices": [],
-    "correct": "April 20-May 20"
+    "choices": ["March 21-April 19","April 20-May 20","June 21-July 22"],
+    "correct": "April 20-May 20",
 },
 {
     "question": "Gemini",
-    "choices": [],
+    "choices": ["March 21-April 19","May 21-June 20","October 23-November 21"],
     "correct": "May 21-June 20",
 },
 {
     "question": "Cancer",
-    "choices": [],
+    "choices": ["July 23-August 22","June 21-July 22","December 22-January 19"],
     "correct": "June 21-July 22",
 },
 {
     "question": "Leo",
-    "choices": [],
+    "choices": ["July 23-August 22","August 23-September 22","March 21-April 19"],
     "correct": "July 23-August 22",
 },
 {
     "question": "Virgo",
-    "choices": [],
+    "choices": ["September 23-October 22","August 23-September 22","October 23-November 21"],
     "correct": "August 23-September 22",
 },
 {
     "question": "Libra",
-    "choices": [],
+    "choices": ["October 23-November 21","Feb 19-March 20","September 23-October 22"],
     "correct": "September 23-October 22",
 },
 {
     "question": "Scorpio",
-    "choices": [],
+    "choices": ["October 23-November 21","November 22-December 21","August 23-September 22"],
     "correct": "October 23-November 21",
 },
 {
     "question": "Sagittarius",
-    "choices": [],
+    "choices": ["November 22-December 21","December 22-January 19","Feb 19-March 20"],
     "correct":"November 22-December 21",
 },
 {
     "question": "Capricorn",
-    "choices": [],
+    "choices": ["July 23-August 22","December 22-January 19","Jan 20-Feb 18"],
     "correct": "December 22-January 19",
 },
 {
     "question": "Aquarius",
-    "choices": [],
+    "choices": ["December 22-January 19","Jan 20-Feb 18","Feb 19-March 20"],
     "correct": "Jan 20-Feb 18",
 },
 {
     "question": "Pisces",
-    "choices": [],
+    "choices": ["Feb 19-March 20","Jan 20-Feb 18","November 22-December 21"],
     "correct":"Feb 19-March 20",
 },
 ];
@@ -64,62 +64,62 @@ const zodiac = [
 const stars = [
 {
     "question": "Aries",
-    "choices": [],
+    "choices": ["Mars","Venus","Mercur"],
     "correct": "Mars",
 },
 {
     "question": "Taurus",
-    "choices": [],
+    "choices": ["Venus","Mars","Mercur"],
     "correct": "Venus",
 },
 {
     "question": "Gemini",
-    "choices": [],
+    "choices": ["Mercur","Moon","Mercur"],
     "correct": "Mercur",
 },
 {
     "question": "Cancer",
-    "choices": [],
+    "choices": ["Moon","Mercur","Venus"],
     "correct": "Moon",
 },
 {
     "question": "Leo",
-    "choices": [],
+    "choices": ["Sun","Venus","Mars"],
     "correct": "Sun",
 },
 {
     "question": "Virgo",
-    "choices": [],
+    "choices": ["Mercur","Sun","Venus"],
     "correct": "Mercur",
 },
 {
     "question": "Libra",
-    "choices": [],
+    "choices": ["Venus","Mercur","Moon"],
     "correct": "Venus",
 },
 {
     "question": "Scorpio",
-    "choices": [],
+    "choices": ["Mars","Venus","Jupiter"],
     "correct": "Mars",
 },
 {
     "question": "Sagittarius",
-    "choices": [],
+    "choices": ["Jupiter","Saturn","Mars"],
     "correct": "Jupiter",
 },
 {
     "question": "Capricorn",
-    "choices": [],
+    "choices": ["Saturn","Uranus","Jupiter"],
     "correct": "Saturn",
 },
 {
     "question": "Aquarius",
-    "choices": [],
+    "choices": ["Uranus","Saturn","Neptun"],
     "correct": "Uranus",
 },
 {
     "question": "Pisces",
-    "choices": [],
+    "choices": ["Neptun","Uranus","Saturn"],
     "correct": "Neptun",
 },
 ]
