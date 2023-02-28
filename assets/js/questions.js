3fb62ab5a79abd5350a21246b373bae9b35a2616
Tamas Baranyai <thamasb@gmail.com>
2023-02-28 12:01:59 +0000

    Add js quiz content

---
+++ assets/js/questions.js
@@ -0,0 +1,125 @@
+const zodiac = [
+{
+    "question": "Aries",
+    "choices": [],
+    "correct":"March 21-April 19"
+},
+{
+    "question": "Taurus",
+    "choices": [],
+    "correct": "April 20-May 20"
+},
+{
+    "question": "Gemini",
+    "choices": [],
+    "correct": "May 21-June 20",
+},
+{
+    "question": "Cancer",
+    "choices": [],
+    "correct": "June 21-July 22",
+},
+{
+    "question": "Leo",
+    "choices": [],
+    "correct": "July 23-August 22",
+},
+{
+    "question": "Virgo",
+    "choices": [],
+    "correct": "August 23-September 22",
+},
+{
+    "question": "Libra",
+    "choices": [],
+    "correct": "September 23-October 22",
+},
+{
+    "question": "Scorpio",
+    "choices": [],
+    "correct": "October 23-November 21",
+},
+{
+    "question": "Sagittarius",
+    "choices": [],
+    "correct":"November 22-December 21",
+},
+{
+    "question": "Capricorn",
+    "choices": [],
+    "correct": "December 22-January 19",
+},
+{
+    "question": "Aquarius",
+    "choices": [],
+    "correct": "Jan 20-Feb 18",
+},
+{
+    "question": "Pisces",
+    "choices": [],
+    "correct":"Feb 19-March 20",
+},
+];
+
+const stars = [
+{
+    "question": "Aries",
+    "choices": [],
+    "correct": "Mars",
+},
+{
+    "question": "Taurus",
+    "choices": [],
+    "correct": "Venus",
+},
+{
+    "question": "Gemini",
+    "choices": [],
+    "correct": "Mercur",
+},
+{
+    "question": "Cancer",
+    "choices": [],
+    "correct": "Moon",
+},
+{
+    "question": "Leo",
+    "choices": [],
+    "correct": "Sun",
+},
+{
+    "question": "Virgo",
+    "choices": [],
+    "correct": "Mercur",
+},
+{
+    "question": "Libra",
+    "choices": [],
+    "correct": "Venus",
+},
+{
+    "question": "Scorpio",
+    "choices": [],
+    "correct": "Mars",
+},
+{
+    "question": "Sagittarius",
+    "choices": [],
+    "correct": "Jupiter",
+},
+{
+    "question": "Capricorn",
+    "choices": [],
+    "correct": "Saturn",
+},
+{
+    "question": "Aquarius",
+    "choices": [],
+    "correct": "Uranus",
+},
+{
+    "question": "Pisces",
+    "choices": [],
+    "correct": "Neptun",
+},
+]
