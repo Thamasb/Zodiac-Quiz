e65c221d8a799c9ba4e483411ea49ed8780aa2b6
Tamas Baranyai <thamasb@gmail.com>
2023-02-28 11:18:38 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let startArea = document.getElementById("welcome");
+let quizArea = document.getElementById("quiz-area");
+let questionText = document.getElementById("question-text");
+let answerButtons = document.getElementById("btn-answer");
+
+let score;
+let currentQuestion;
+
+function startQuiz(){
+
+}
+
+function displayQuestion(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+    
+}
