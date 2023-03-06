9daa16488ca8e290a73c4902e4f0186effe3e4c4
Tamas Baranyai <thamasb@gmail.com>
2023-03-06 19:47:05 +0000

    Add code for random questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,3 +90,13 @@ function restartQuiz(){
     hideAllSections();
     startArea.classList.add("active");
 }
+
+//randomly shuffle an array
+function shuffleArray(array){
+    for (let i = array.length -1; i>0; i--){
+        const j = Math.floor(Math.random()*(i+1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
