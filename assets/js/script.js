622e29ed7ce50c06c752df8a06a6416fe4495e06
Tamas Baranyai <thamasb@gmail.com>
2023-03-09 17:23:13 +0000

    Fix the warnings in javasript file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,33 @@
-let startArea = document.getElementById("welcome");
-let quizArea = document.getElementById("quiz-area");
-let questionText = document.getElementById("question-text");
-let answerButtons = document.getElementsByClassName("btn-answer");
-let nextButton = document.getElementById("btn-next");
-let quizResult = document.getElementById("quiz-result");
-let scoreEl = document.getElementById("score");
-let incorrectEl = document.getElementById("incorrect");
-let quizProgressEl = document.getElementById("quiz-progress");
-let sectionQuizEl = document.getElementById("section-quiz");
-let resultCommentEl = document.getElementById("result-comment");
-let usernameInputEl = document.getElementById("username");
-let usernameOutputEl = document.getElementById("username-output");
+var startArea = document.getElementById("welcome");
+var quizArea = document.getElementById("quiz-area");
+var questionText = document.getElementById("question-text");
+var answerButtons = document.getElementsByClassName("btn-answer");
+var nextButton = document.getElementById("btn-next");
+var quizResult = document.getElementById("quiz-result");
+var scoreEl = document.getElementById("score");
+var incorrectEl = document.getElementById("incorrect");
+var quizProgressEl = document.getElementById("quiz-progress");
+var sectionQuizEl = document.getElementById("section-quiz");
+var resultCommentEl = document.getElementById("result-comment");
+var usernameInputEl = document.getElementById("username");
+var usernameOutputEl = document.getElementById("username-output");
 var quizFeedbackEl = document.getElementById("quiz-feedback");
 
 const noUsername = document.getElementById('no-username');
 
-let score;
-let incorrect;
-let currentQuestion;
-let questionSet;
+var score;
+var incorrect;
+var currentQuestion;
+var questionSet;
 
-startArea.classList.add("active");
+//startArea.classList.add("active");//
 //Start quiz game with questions and name provided, set the selected questons, reset the points
 function startQuiz(questions, name) {
-    if (usernameInputEl.value.length === 0) {
-        noUsername.style.display = 'block';
-        return;
-    }
+    if (!questions && !name) return;
+    //if (usernameInputEl.value.length === 0){
+    //noUsername.style.display = 'block';
+    // return;
+    // }
     score = 0;
     incorrect = 0;
     currentQuestion = 0;
@@ -46,13 +47,21 @@ function displayQuestion() {
         return;
     }
     questionText.innerHTML = questionSet[currentQuestion].question;
-    let questionNumber = currentQuestion +1;
+    var questionNumber = currentQuestion + 1;
     quizProgressEl.innerHTML = questionNumber + " of " + questionSet.length;
-    for (let i = 0; i + 1 <= answerButtons.length; i++) {
-        answerButtons[i].classList.remove("correct");
-        answerButtons[i].classList.remove("wrong");
-        answerButtons[i].innerHTML = questionSet[currentQuestion].choices[i];
-        answerButtons[i].onclick = function () {
+    Array.from(answerButtons).forEach(function (btn, i) {
+        btn.classList.remove("correct");
+        btn.classList.remove("wrong");
+        btn.innerHTML = questionSet[currentQuestion].choices[i];
+        // }
+        //for (let i = 0; i + 1 <= answerButtons.length; i++) {
+        // answerButtons[i].classList.remove("correct");
+        // answerButtons[i].classList.remove("wrong");
+        // answerButtons[i].innerHTML = questionSet[currentQuestion].choices[i];
+        // answerButtons[i].onclick = function () {
+        //     checkAnswer(this);
+        //  };
+        btn.onclick = function () {
             checkAnswer(this);
         };
         nextButton.onclick = function () {
@@ -60,21 +69,23 @@ function displayQuestion() {
             nextButton.style.display = "none";
             displayQuestion();
         };
-    }
+    });
 }
+
+
 //Check answer button when clicked, shows the correct and incorrect answers
 function checkAnswer(btn) {
-    let correctAns = questionSet[currentQuestion].correct;
+    var correctAns = questionSet[currentQuestion].correct;
     if (correctAns === btn.innerHTML) {
         btn.classList.add("correct");
         incrementScore();
     } else {
-        let correctIndex = questionSet[currentQuestion].choices.indexOf(correctAns);
+        var correctIndex = questionSet[currentQuestion].choices.indexOf(correctAns);
         btn.classList.add("wrong");
         answerButtons[correctIndex].classList.add("correct");
         incrementWrongAnswer();
     }
-    for (let i = 0; i + 1 <= answerButtons.length; i++) {
+    for (var i = 0; i + 1 <= answerButtons.length; i++) {
         answerButtons[i].onclick = null;
     }
     nextButton.style.display = "block";
@@ -91,22 +102,22 @@ function incrementWrongAnswer() {
 }
 
 
-function hideAllSections(){
+function hideAllSections() {
     startArea.classList.remove("active");
     quizArea.classList.remove("active");
     quizResult.classList.remove("active");
 }
 //Hide all screen and show the result screen (player neme, comment, score)
-function gameOver(){
+function gameOver() {
     hideAllSections();
     quizResult.classList.add("active");
-    let comment = "";
+    var comment = "";
 
     if (score >= 12) {
         comment = "Congratulation you are excellent in astrology.";
-    } else if (score > 6){
+    } else if (score > 6) {
         comment = "You are good in astrology.";
-    } else if ( score >0){
+    } else if (score > 0) {
         comment = "You are doing well, try it again.";
     } else {
         comment = "Don’t cry, try it again.";
@@ -121,7 +132,7 @@ function gameOver(){
 }
 
 
-function restartQuiz(){
+function restartQuiz() {
     hideAllSections();
     sectionQuizEl.classList.remove("zodiac");
     sectionQuizEl.classList.remove("stars");
@@ -129,21 +140,21 @@ function restartQuiz(){
 }
 
 //randomly shuffle an array
-function shuffleArray(array){
-    for (let i = array.length -1; i>0; i--){
-        const j = Math.floor(Math.random()*(i+1));
-        const temp = array[i];
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
 }
 
-function openFeedback(){
+function openFeedback() {
     hideAllSections();
     quizFeedbackEl.classList.add("active");
 }
 
-window.addEventListener("DOMContentLoaded", function (){
+window.addEventListener("DOMContentLoaded", function () {
     startQuiz();
     restartQuiz();
 });
